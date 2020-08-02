c9d1ed94e8234a933807892aea213fa7bd2ccd74
Joris Quist <Joris Quist>
2020-08-02 15:00:58 +0200

    Added test or coordinate system conversion

---
+++ test/test_coordinate_system_conversion.py
@@ -0,0 +1,37 @@
+import math
+import unittest
+import numpy as np
+
+from lobster_common.quaternion import Quaternion
+from lobster_common.vec3 import Vec3
+
+
+class CoordinateSystemConversionTest(unittest.TestCase):
+
+    def test_conversion(self):
+        # Added seed to make tests with np.random deterministic
+        np.random.seed(0)
+
+        for _ in range(50):
+            # Creating random vector
+            vec = Vec3(np.random.rand(3))
+
+            # Creating random rotation
+            q = Quaternion.from_euler(Vec3(np.random.rand(3) * 2 * math.pi))
+
+            # Rotate the vector by the rotation
+            rotated_vec = vec.rotate(q)
+
+            # transform the original vector to the NED coordinate system
+            vec_NED = Vec3.fromENU(vec)
+
+            # transform the original quaternino to the NED coordinate system
+            q_NED = Quaternion.fromENU(q)
+
+            # Rotate the converted vector by the converted rotation
+            rotated_vec_NED = vec_NED.rotate(q_NED)
+
+            # transform the rotated vector to the NED coordinate system
+            rotated_vec_NED_transformed = Vec3.fromENU(rotated_vec)
+
+            self.assertEqual(rotated_vec_NED, rotated_vec_NED_transformed)
