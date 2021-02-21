7c354510a6e5330d38a98e97a5e13b2c78238c16
Danny Looman <dannylooman@gmail.com>
2021-02-21 16:43:30 +0100

    changed test such that the difference in magnitude must be small.

--- test/test_vec3.py
+++ test/test_vec3.py
@@ -19,7 +19,10 @@ class Vec3Test(unittest.TestCase):
             rotation = quaternion.Quaternion.from_euler(vec3.Vec3(np.random.rand(3) * 4 * math.pi - 2 * math.pi))
 
             numpy_method = vec3.Vec3(rotation.get_rotation_matrix().dot(vec.numpy()))
-
             rotate_method = vec.rotate(rotation)
 
-            self.assertEqual(numpy_method, rotate_method)
+            numpy_method_inv = vec3.Vec3(rotation.get_inverse_rotation_matrix().dot(vec.numpy()))
+            rotate_method_inv = vec.rotate_inverse(rotation)
+
+            self.assertTrue(np.linalg.norm((numpy_method - rotate_method).numpy()) < 0.00000000001)
+            self.assertTrue(np.linalg.norm((numpy_method_inv - rotate_method_inv).numpy()) < 0.00000000001)
